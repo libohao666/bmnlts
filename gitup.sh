0acc0b1f3022273ed9b2bb01f91297163f348730
libohao666 <2052658718@qq.com>
2019-12-28 16:03:30 +0800

    20191228

--- gitup.sh
+++ gitup.sh
@@ -5,7 +5,9 @@
 # Created Time: 2019年12月28日 星期六 15时49分00秒
 #########################################################################
 #!/bin/bash
+#!/bin/expect
 git add *
 git commit -m $(date "+%Y%m%d")
 git push origin master
+expect "Username for 'https://github.com':''"
 send "libohao666"
